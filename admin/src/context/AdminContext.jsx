1a40f6cbebcb2e55038a2535ce56dde71429b09d
Abhishek <154580792+krishnabhishek1234@users.noreply.github.com>
2025-10-03 21:16:44 +0530

    Adding user

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -26,9 +26,24 @@ const AdminContextProvider = (props) => {
         }
     }
 
+
+    const changeAvailability = async (docId) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, { headers: { aToken } })
+            if (data.success) {
+                toast.success(data.message)
+                getAllDoctors()
+            } else {
+                toast.error(error.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken, setAToken,
-        backendUrl, doctors, getAllDoctors
+        backendUrl, doctors, getAllDoctors, changeAvailability
     }
     return (
         <AdminContext.Provider value={value}>
